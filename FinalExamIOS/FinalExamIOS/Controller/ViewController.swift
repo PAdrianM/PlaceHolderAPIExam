cf2f7cc15c019f05310115c778ea16b319172b53
Adrián Ismael Orlando García Balan <3615924770101@ingenieria.usac.edu.gt>
2024-01-31 10:42:09 -0600

    Initial Commit 2

---
+++ FinalExamIOS/FinalExamIOS/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalExamIOS
+//
+//  Created by Andrea Hernandez on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
